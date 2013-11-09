8ad2a5c848ac134e80795942a62c8acc38543798
dotcom900825 <dotcom900825@gmail.com>
2013-11-08 23:51:33 -0800

    initial register function

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,5 +1,24 @@
 class SuppliersController < ApplicationController
   def new
-
+    @supplier = Supplier.new
   end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+
+    respond_to do |format|
+      if @supplier.save
+        auto_login @supplier
+        format.html { redirect_to root_path, notice: 'Welcome!' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  private
+  def supplier_params
+    params.require(:supplier).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
